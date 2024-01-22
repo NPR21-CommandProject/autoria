85ecd3ade7fe6eb8ea4b93aa32691c20ffa67fa0
alekyou17 <AlekSamkov@gmail.com>
2024-01-22 13:23:45 +0200

    Beta viev of main page

--- AutoriaWinForm/MainForm.Designer.cs
+++ AutoriaWinForm/MainForm.Designer.cs
@@ -28,12 +28,303 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            vScrollBar1 = new VScrollBar();
+            Signin_btn = new Button();
+            Signup_btn = new Button();
+            Exit = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox2 = new ComboBox();
+            label4 = new Label();
+            comboBox3 = new ComboBox();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox4 = new ComboBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            vScrollBar1.Location = new Point(1395, 9);
+            vScrollBar1.Name = "vScrollBar1";
+            vScrollBar1.Size = new Size(17, 601);
+            vScrollBar1.TabIndex = 2;
+            // 
+            // Signin_btn
+            // 
+            Signin_btn.Location = new Point(12, 9);
+            Signin_btn.Name = "Signin_btn";
+            Signin_btn.Size = new Size(132, 30);
+            Signin_btn.TabIndex = 3;
+            Signin_btn.Text = "Увійти";
+            Signin_btn.UseVisualStyleBackColor = true;
+            // 
+            // Signup_btn
+            // 
+            Signup_btn.Location = new Point(150, 9);
+            Signup_btn.Name = "Signup_btn";
+            Signup_btn.Size = new Size(132, 30);
+            Signup_btn.TabIndex = 4;
+            Signup_btn.Text = "Зареєструватися";
+            Signup_btn.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            Exit.Location = new Point(1260, 9);
+            Exit.Name = "Exit";
+            Exit.Size = new Size(132, 30);
+            Exit.TabIndex = 5;
+            Exit.Text = "Вихід";
+            Exit.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(903, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(288, 30);
+            button1.TabIndex = 6;
+            button1.Text = "Продати авто";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(520, 9);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(291, 30);
+            textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(388, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 30);
+            label1.TabIndex = 8;
+            label1.Text = "Пошук авто";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 12F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 141);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(217, 29);
+            comboBox1.TabIndex = 9;
+            comboBox1.Text = "Усі типи транспорту";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 21);
+            label2.TabIndex = 10;
+            label2.Text = "Тип транспорту";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 177);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 21);
+            label3.TabIndex = 12;
+            label3.Text = "Марка";
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Segoe UI", 12F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 201);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(217, 29);
+            comboBox2.TabIndex = 11;
+            comboBox2.Text = "Оберіть";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(12, 240);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 21);
+            label4.TabIndex = 14;
+            label4.Text = "Модель";
+            // 
+            // comboBox3
+            // 
+            comboBox3.Font = new Font("Segoe UI", 12F);
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(12, 264);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(217, 29);
+            comboBox3.TabIndex = 13;
+            comboBox3.Text = "Оберіть";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(12, 303);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 21);
+            label5.TabIndex = 15;
+            label5.Text = "Ціна";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(12, 327);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(70, 29);
+            textBox2.TabIndex = 16;
+            textBox2.Text = "від";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F);
+            textBox3.Location = new Point(88, 327);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(70, 29);
+            textBox3.TabIndex = 17;
+            textBox3.Text = "до";
+            // 
+            // comboBox4
+            // 
+            comboBox4.Font = new Font("Segoe UI", 12F);
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(164, 327);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(65, 29);
+            comboBox4.TabIndex = 18;
+            comboBox4.Text = "$";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox4.Location = new Point(126, 392);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(103, 29);
+            textBox4.TabIndex = 21;
+            textBox4.Text = "до";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox5.Location = new Point(12, 392);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(108, 29);
+            textBox5.TabIndex = 20;
+            textBox5.Text = "від";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.Location = new Point(12, 368);
+            label6.Name = "label6";
+            label6.Size = new Size(132, 21);
+            label6.TabIndex = 19;
+            label6.Text = "Пробіг (тис. км.)";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox6.Location = new Point(126, 456);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(103, 29);
+            textBox6.TabIndex = 24;
+            textBox6.Text = "до";
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox7.Location = new Point(12, 456);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(108, 29);
+            textBox7.TabIndex = 23;
+            textBox7.Text = "від";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.Location = new Point(12, 432);
+            label7.Name = "label7";
+            label7.Size = new Size(145, 21);
+            label7.TabIndex = 22;
+            label7.Text = "Об'єм двигуна (л.)";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1421, 619);
+            Controls.Add(textBox6);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(comboBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(comboBox3);
+            Controls.Add(label3);
+            Controls.Add(comboBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(Exit);
+            Controls.Add(Signup_btn);
+            Controls.Add(Signin_btn);
+            Controls.Add(vScrollBar1);
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private VScrollBar vScrollBar1;
+        private Button Signin_btn;
+        private Button Signup_btn;
+        private Button Exit;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox2;
+        private Label label4;
+        private ComboBox comboBox3;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox4;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label7;
     }
 }
--- AutoriaWinForm/MainForm.cs
+++ AutoriaWinForm/MainForm.cs
@@ -6,5 +6,7 @@ namespace AutoriaWinForm
         {
             InitializeComponent();
         }
+
+        
     }
 }
