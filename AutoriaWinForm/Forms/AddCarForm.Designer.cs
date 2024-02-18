39261e5edd57719295186140967257d71da120de
novakvova <novakvova@gmail.com>
2024-02-18 10:10:38 +0200

    Begin simple view images

--- AutoriaWinForm/Forms/AddCarForm.Designer.cs
+++ AutoriaWinForm/Forms/AddCarForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCarForm));
-            txtBoxPhotoDecription = new TextBox();
             vehicleTipeBox = new ComboBox();
             label1 = new Label();
             vehicleMarkBox = new ComboBox();
@@ -51,21 +50,9 @@
             txtBoxOdometer = new TextBox();
             txtBoxCapacity = new TextBox();
             txtBoxFuelType = new TextBox();
+            lvImages = new ListView();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // txtBoxPhotoDecription
-            // 
-            txtBoxPhotoDecription.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            txtBoxPhotoDecription.ForeColor = Color.Gray;
-            txtBoxPhotoDecription.Location = new Point(392, 117);
-            txtBoxPhotoDecription.Multiline = true;
-            txtBoxPhotoDecription.Name = "txtBoxPhotoDecription";
-            txtBoxPhotoDecription.Size = new Size(775, 184);
-            txtBoxPhotoDecription.TabIndex = 1;
-            txtBoxPhotoDecription.Text = "Photo decription";
-            txtBoxPhotoDecription.Click += txtBoxPhotoDecription_Click;
-            txtBoxPhotoDecription.TextChanged += txtPhotoDescription_TextChanged_1;
             // 
             // vehicleTipeBox
             // 
@@ -75,9 +62,10 @@
             vehicleTipeBox.ForeColor = Color.Gray;
             vehicleTipeBox.FormattingEnabled = true;
             vehicleTipeBox.Items.AddRange(new object[] { "Passenger cars", "", "Motorcycles", "", "Trucks", "", "Trailers", "", "Special equipment", "", "Agricultural machinery", "", "Buses", "", "Water transport", "", "Air transport", "", "Motor homes" });
-            vehicleTipeBox.Location = new Point(34, 294);
+            vehicleTipeBox.Location = new Point(39, 392);
+            vehicleTipeBox.Margin = new Padding(3, 4, 3, 4);
             vehicleTipeBox.Name = "vehicleTipeBox";
-            vehicleTipeBox.Size = new Size(273, 23);
+            vehicleTipeBox.Size = new Size(311, 28);
             vehicleTipeBox.TabIndex = 2;
             vehicleTipeBox.Text = "All vehicle";
             vehicleTipeBox.SelectedIndexChanged += vehicleTipeBox_SelectedIndexChanged;
@@ -88,9 +76,9 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(34, 270);
+            label1.Location = new Point(39, 360);
             label1.Name = "label1";
-            label1.Size = new Size(104, 21);
+            label1.Size = new Size(128, 28);
             label1.TabIndex = 3;
             label1.Text = "Vehicle type";
             label1.Click += label1_Click;
@@ -101,9 +89,10 @@
             vehicleMarkBox.ForeColor = Color.Gray;
             vehicleMarkBox.FormattingEnabled = true;
             vehicleMarkBox.Items.AddRange(new object[] { "BMW", "Ford", "Toyota", "Chevrolet", "Honda", "Hyundai", "Volkswagen", "Audi", "Bentley", "Dodge", "Nissan", "Daimler AG", "Ferrari", "Fisker", "Jeep", "Mazda", "Subaru", "Acura", "American Motors", "Bugatti", "Cadillac", "Chrysler", "DeLorean", "Geely" });
-            vehicleMarkBox.Location = new Point(34, 341);
+            vehicleMarkBox.Location = new Point(39, 455);
+            vehicleMarkBox.Margin = new Padding(3, 4, 3, 4);
             vehicleMarkBox.Name = "vehicleMarkBox";
-            vehicleMarkBox.Size = new Size(273, 23);
+            vehicleMarkBox.Size = new Size(311, 28);
             vehicleMarkBox.TabIndex = 4;
             vehicleMarkBox.Text = "All brand";
             vehicleMarkBox.SelectedIndexChanged += vehicleMarkBox_SelectedIndexChanged_1;
@@ -114,9 +103,9 @@
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(34, 317);
+            label2.Location = new Point(39, 423);
             label2.Name = "label2";
-            label2.Size = new Size(84, 21);
+            label2.Size = new Size(104, 28);
             label2.TabIndex = 5;
             label2.Text = "Car brand";
             // 
@@ -126,9 +115,9 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(34, 365);
+            label3.Location = new Point(39, 487);
             label3.Name = "label3";
-            label3.Size = new Size(59, 21);
+            label3.Size = new Size(72, 28);
             label3.TabIndex = 7;
             label3.Text = "Model";
             // 
@@ -137,9 +126,10 @@
             vehicleModelBox.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
             vehicleModelBox.ForeColor = Color.Gray;
             vehicleModelBox.FormattingEnabled = true;
-            vehicleModelBox.Location = new Point(34, 389);
+            vehicleModelBox.Location = new Point(39, 519);
+            vehicleModelBox.Margin = new Padding(3, 4, 3, 4);
             vehicleModelBox.Name = "vehicleModelBox";
-            vehicleModelBox.Size = new Size(273, 23);
+            vehicleModelBox.Size = new Size(311, 28);
             vehicleModelBox.TabIndex = 6;
             vehicleModelBox.Text = "All models";
             vehicleModelBox.SelectedIndexChanged += vehicleModelBox_SelectedIndexChanged_1;
@@ -150,9 +140,9 @@
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(34, 416);
+            label4.Location = new Point(39, 555);
             label4.Name = "label4";
-            label4.Size = new Size(48, 21);
+            label4.Size = new Size(59, 28);
             label4.TabIndex = 8;
             label4.Text = "Price";
             // 
@@ -162,9 +152,9 @@
             label5.BackColor = Color.Transparent;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(34, 475);
+            label5.Location = new Point(39, 633);
             label5.Name = "label5";
-            label5.Size = new Size(87, 21);
+            label5.Size = new Size(107, 28);
             label5.TabIndex = 12;
             label5.Text = "Odometer";
             // 
@@ -174,9 +164,9 @@
             label6.BackColor = Color.Transparent;
             label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(34, 530);
+            label6.Location = new Point(39, 707);
             label6.Name = "label6";
-            label6.Size = new Size(131, 21);
+            label6.Size = new Size(161, 28);
             label6.TabIndex = 18;
             label6.Text = "Engine capacity";
             // 
@@ -184,10 +174,11 @@
             // 
             txtBoxCarDecription.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             txtBoxCarDecription.ForeColor = Color.Gray;
-            txtBoxCarDecription.Location = new Point(392, 355);
+            txtBoxCarDecription.Location = new Point(448, 473);
+            txtBoxCarDecription.Margin = new Padding(3, 4, 3, 4);
             txtBoxCarDecription.Multiline = true;
             txtBoxCarDecription.Name = "txtBoxCarDecription";
-            txtBoxCarDecription.Size = new Size(775, 183);
+            txtBoxCarDecription.Size = new Size(885, 243);
             txtBoxCarDecription.TabIndex = 21;
             txtBoxCarDecription.Text = "Car description";
             txtBoxCarDecription.Click += txtBoxCarDecription_Click;
@@ -197,9 +188,10 @@
             // 
             btnAccept.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             btnAccept.ForeColor = Color.Green;
-            btnAccept.Location = new Point(392, 609);
+            btnAccept.Location = new Point(448, 812);
+            btnAccept.Margin = new Padding(3, 4, 3, 4);
             btnAccept.Name = "btnAccept";
-            btnAccept.Size = new Size(107, 47);
+            btnAccept.Size = new Size(122, 63);
             btnAccept.TabIndex = 22;
             btnAccept.Text = "Confirm";
             btnAccept.UseVisualStyleBackColor = true;
@@ -209,9 +201,10 @@
             // 
             btnCancel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             btnCancel.ForeColor = Color.Red;
-            btnCancel.Location = new Point(1056, 619);
+            btnCancel.Location = new Point(1207, 825);
+            btnCancel.Margin = new Padding(3, 4, 3, 4);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(111, 47);
+            btnCancel.Size = new Size(127, 63);
             btnCancel.TabIndex = 23;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = true;
@@ -220,9 +213,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(34, 117);
+            pictureBox1.Location = new Point(39, 156);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(273, 145);
+            pictureBox1.Size = new Size(312, 193);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 32;
             pictureBox1.TabStop = false;
@@ -234,9 +228,9 @@
             label7.BackColor = Color.Transparent;
             label7.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(602, 13);
+            label7.Location = new Point(688, 17);
             label7.Name = "label7";
-            label7.Size = new Size(438, 65);
+            label7.Size = new Size(547, 81);
             label7.TabIndex = 33;
             label7.Text = "Added car for sale";
             label7.Click += label7_Click_1;
@@ -247,9 +241,9 @@
             label8.BackColor = Color.Transparent;
             label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label8.ForeColor = Color.White;
-            label8.Location = new Point(602, 676);
+            label8.Location = new Point(688, 901);
             label8.Name = "label8";
-            label8.Size = new Size(335, 17);
+            label8.Size = new Size(432, 23);
             label8.TabIndex = 34;
             label8.Text = "(с)This site was developed by a team of young tigers";
             // 
@@ -259,9 +253,9 @@
             label9.BackColor = Color.Transparent;
             label9.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label9.ForeColor = Color.White;
-            label9.Location = new Point(77, 25);
+            label9.Location = new Point(88, 33);
             label9.Name = "label9";
-            label9.Size = new Size(196, 50);
+            label9.Size = new Size(246, 62);
             label9.TabIndex = 35;
             label9.Text = "Car Filtres";
             // 
@@ -271,9 +265,9 @@
             label10.BackColor = Color.Transparent;
             label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label10.ForeColor = Color.White;
-            label10.Location = new Point(34, 589);
+            label10.Location = new Point(39, 785);
             label10.Name = "label10";
-            label10.Size = new Size(80, 21);
+            label10.Size = new Size(99, 28);
             label10.TabIndex = 36;
             label10.Text = "Fuel type";
             // 
@@ -281,10 +275,11 @@
             // 
             txtBoxPrice.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             txtBoxPrice.ForeColor = Color.Gray;
-            txtBoxPrice.Location = new Point(34, 440);
+            txtBoxPrice.Location = new Point(39, 587);
+            txtBoxPrice.Margin = new Padding(3, 4, 3, 4);
             txtBoxPrice.Multiline = true;
             txtBoxPrice.Name = "txtBoxPrice";
-            txtBoxPrice.Size = new Size(273, 32);
+            txtBoxPrice.Size = new Size(311, 41);
             txtBoxPrice.TabIndex = 38;
             txtBoxPrice.Text = "in uah";
             txtBoxPrice.TextChanged += txtBoxPrice_TextChanged_1;
@@ -293,10 +288,11 @@
             // 
             txtBoxOdometer.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             txtBoxOdometer.ForeColor = Color.Gray;
-            txtBoxOdometer.Location = new Point(34, 495);
+            txtBoxOdometer.Location = new Point(39, 660);
+            txtBoxOdometer.Margin = new Padding(3, 4, 3, 4);
             txtBoxOdometer.Multiline = true;
             txtBoxOdometer.Name = "txtBoxOdometer";
-            txtBoxOdometer.Size = new Size(273, 32);
+            txtBoxOdometer.Size = new Size(311, 41);
             txtBoxOdometer.TabIndex = 39;
             txtBoxOdometer.Text = "in km";
             txtBoxOdometer.TextChanged += txtBoxOdometer_TextChanged;
@@ -305,10 +301,11 @@
             // 
             txtBoxCapacity.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             txtBoxCapacity.ForeColor = Color.Gray;
-            txtBoxCapacity.Location = new Point(34, 554);
+            txtBoxCapacity.Location = new Point(39, 739);
+            txtBoxCapacity.Margin = new Padding(3, 4, 3, 4);
             txtBoxCapacity.Multiline = true;
             txtBoxCapacity.Name = "txtBoxCapacity";
-            txtBoxCapacity.Size = new Size(273, 32);
+            txtBoxCapacity.Size = new Size(311, 41);
             txtBoxCapacity.TabIndex = 40;
             txtBoxCapacity.Text = "in litres";
             txtBoxCapacity.TextChanged += txtBoxCapacity_TextChanged;
@@ -317,21 +314,36 @@
             // 
             txtBoxFuelType.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             txtBoxFuelType.ForeColor = Color.Gray;
-            txtBoxFuelType.Location = new Point(34, 619);
+            txtBoxFuelType.Location = new Point(39, 825);
+            txtBoxFuelType.Margin = new Padding(3, 4, 3, 4);
             txtBoxFuelType.Multiline = true;
             txtBoxFuelType.Name = "txtBoxFuelType";
-            txtBoxFuelType.Size = new Size(273, 32);
+            txtBoxFuelType.Size = new Size(311, 41);
             txtBoxFuelType.TabIndex = 41;
             txtBoxFuelType.Text = "input type";
             txtBoxFuelType.TextChanged += txtBoxFuelType_TextChanged;
             // 
+            // lvImages
+            // 
+            lvImages.Location = new Point(448, 156);
+            lvImages.Name = "lvImages";
+            lvImages.Size = new Size(885, 273);
+            lvImages.TabIndex = 42;
+            lvImages.UseCompatibleStateImageBehavior = false;
+            lvImages.ItemDrag += lvImages_ItemDrag;
+            lvImages.DragDrop += lvImages_DragDrop;
+            lvImages.DragEnter += lvImages_DragEnter;
+            lvImages.DragOver += lvImages_DragOver;
+            lvImages.DragLeave += lvImages_DragLeave;
+            // 
             // AddCarForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Orange;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1280, 720);
+            ClientSize = new Size(1463, 960);
+            Controls.Add(lvImages);
             Controls.Add(txtBoxFuelType);
             Controls.Add(txtBoxCapacity);
             Controls.Add(txtBoxOdometer);
@@ -353,9 +365,10 @@
             Controls.Add(vehicleMarkBox);
             Controls.Add(label1);
             Controls.Add(vehicleTipeBox);
-            Controls.Add(txtBoxPhotoDecription);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AddCarForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "AddCarForm";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
@@ -365,7 +378,6 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private TextBox txtBoxPhotoDecription;
         private ComboBox vehicleTipeBox;
         private Label label1;
         private ComboBox vehicleMarkBox;
@@ -391,5 +403,6 @@
         private TextBox txtBoxOdometer;
         private TextBox txtBoxCapacity;
         private TextBox txtBoxFuelType;
+        private ListView lvImages;
     }
 }
--- AutoriaWinForm/Forms/AddCarForm.cs
+++ AutoriaWinForm/Forms/AddCarForm.cs
@@ -11,6 +11,8 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using AutoriaWinForm.Data.Entities;
 using Autoria.Helpers;
 using AutoriaWinForm.Data;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System.IO;
 
 
 namespace AutoriaWinForm.Forms
@@ -21,11 +23,112 @@ namespace AutoriaWinForm.Forms
     {
 
         private CarEntity newCar;
+
+        private List<string> ProductImages { get; set; }
+
+        private class ListViewIndexComparer : System.Collections.IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                return ((ListViewItem)x).Index - ((ListViewItem)y).Index;
+            }
+        }
+
         public AddCarForm()
         {
             InitializeComponent();
+            lvImages.LargeImageList = new ImageList();
+            lvImages.LargeImageList.ImageSize = new Size(90, 65);
+           // lvImages.LargeImageList.
+            lvImages.MultiSelect = false;
+            lvImages.ListViewItemSorter = new ListViewIndexComparer();
+            lvImages.InsertionMark.Color = Color.Green;
+            lvImages.AllowDrop = true;
+
             newCar = new CarEntity();
         }
+
+        #region Drag and Drop Images in ListView
+        private void lvImages_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            lvImages.DoDragDrop(e.Item, DragDropEffects.Move);
+        }
+
+        private void lvImages_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void lvImages_DragLeave(object sender, EventArgs e)
+        {
+            lvImages.InsertionMark.Index = -1;
+        }
+
+        private void lvImages_DragOver(object sender, DragEventArgs e)
+        {
+            // Retrieve the client coordinates of the mouse pointer.
+            Point targetPoint =
+                lvImages.PointToClient(new Point(e.X, e.Y));
+
+            // Retrieve the index of the item closest to the mouse pointer.
+            int targetIndex = lvImages.InsertionMark.NearestIndex(targetPoint);
+
+            // Confirm that the mouse pointer is not over the dragged item.
+            if (targetIndex > -1)
+            {
+                // Determine whether the mouse pointer is to the left or
+                // the right of the midpoint of the closest item and set
+                // the InsertionMark.AppearsAfterItem property accordingly.
+                Rectangle itemBounds = lvImages.GetItemRect(targetIndex);
+                if (targetPoint.X > itemBounds.Left + (itemBounds.Width / 2))
+                {
+                    lvImages.InsertionMark.AppearsAfterItem = true;
+                }
+                else
+                {
+                    lvImages.InsertionMark.AppearsAfterItem = false;
+                }
+            }
+
+            // Set the location of the insertion mark. If the mouse is
+            // over the dragged item, the targetIndex value is -1 and
+            // the insertion mark disappears.
+            lvImages.InsertionMark.Index = targetIndex;
+        }
+
+        private void lvImages_DragDrop(object sender, DragEventArgs e)
+        {
+            // Retrieve the index of the insertion mark;
+            int targetIndex = lvImages.InsertionMark.Index;
+
+            // If the insertion mark is not visible, exit the method.
+            if (targetIndex == -1)
+            {
+                return;
+            }
+
+            // If the insertion mark is to the right of the item with
+            // the corresponding index, increment the target index.
+            if (lvImages.InsertionMark.AppearsAfterItem)
+            {
+                targetIndex++;
+            }
+
+            // Retrieve the dragged item.
+            ListViewItem draggedItem =
+                (ListViewItem)e.Data.GetData(typeof(ListViewItem));
+
+            // Insert a copy of the dragged item at the target index.
+            // A copy must be inserted before the original item is removed
+            // to preserve item index values. 
+            lvImages.Items.Insert(
+                targetIndex, (ListViewItem)draggedItem.Clone());
+
+            // Remove the original copy of the dragged item.
+            lvImages.Items.Remove(draggedItem);
+        }
+        #endregion
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -80,31 +183,6 @@ namespace AutoriaWinForm.Forms
             newCar.Model = selectedVehicleModel;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged_2(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnCancel_Click(object sender, EventArgs e)
         {
             Close();
@@ -134,13 +212,6 @@ namespace AutoriaWinForm.Forms
             newCar.FuelType = selectedVehicleFuelType;
         }
 
-        private void txtPhotoDescription_TextChanged_1(object sender, EventArgs e)
-        {
-
-            string inputData = txtBoxPhotoDecription.Text;
-            newCar.PhotoDescription = inputData;
-        }
-
         private void txtCarDescription_TextChanged_1(object sender, EventArgs e)
         {
             string inputData = txtBoxCarDecription.Text;
@@ -156,7 +227,16 @@ namespace AutoriaWinForm.Forms
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string imagePath = openFileDialog.FileName;
-                    pictureBox1.Image=Image.FromFile(imagePath);
+                    string key = Guid.NewGuid().ToString();
+                    ListViewItem item = new ListViewItem();
+                    item.Tag = imagePath;
+                    item.Text = Path.GetFileName(imagePath);
+                    item.ImageKey = key;
+                    var image = SixLabors.ImageSharp.Image.Load(imagePath);
+                    MemoryStream ms = new MemoryStream();
+                    image.Save(ms, new JpegEncoder());
+                    lvImages.LargeImageList.Images.Add(key, Image.FromStream(ms));
+                    lvImages.Items.Add(item);
                 }
             }
         }
@@ -185,11 +265,6 @@ namespace AutoriaWinForm.Forms
             {
                 MessageBox.Show($"Помилка при збереженні даних про автомобіль: {ex.Message}");
             }
-        }
-
-        private void txtBoxPhotoDecription_Click(object sender, EventArgs e)
-        {
-            txtBoxPhotoDecription.Text = string.Empty;
         }
 
         private void txtBoxCarDecription_Click(object sender, EventArgs e)
